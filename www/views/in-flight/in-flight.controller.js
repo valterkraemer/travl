e9a90e9b36f844edf1cea0a4613ebb7c10e8ecdc
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2015-11-07 20:51:38 +0200

    Style notifications, add static content. Create transfer logic from notifications to next flight view.

--- www/views/in-flight/in-flight.controller.js
+++ www/views/in-flight/in-flight.controller.js
@@ -1,6 +1,6 @@
 angular.module('starter')
 
-.controller('InFlightController', function($scope, $ionicModal) {
+.controller('InFlightController', function($scope, $ionicModal, $state) {
 
   $ionicModal.fromTemplateUrl('views/in-flight/notification.html', {
     scope: $scope,
@@ -8,8 +8,14 @@ angular.module('starter')
   }).then(function(modal) {
     $scope.modal = modal;
   });
+
   $scope.showNotification = function() {
     $scope.modal.show();
-  }
+  };
+
+  $scope.goToNextFlightInfo = function() {
+    $scope.modal.hide();
+    $state.go('tab.nextFlight');
+  };
 
 });
