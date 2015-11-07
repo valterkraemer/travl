b95baa6689da707b342fe110ac48b061aa3d1249
Ville Miekk-oja <VilleMiekkoja@gmail.com>
2015-11-07 22:07:48 +0200

    Connect notification flight to open flight view with correct data.

--- www/views/in-flight/in-flight.controller.js
+++ www/views/in-flight/in-flight.controller.js
@@ -1,6 +1,6 @@
 angular.module('starter')
 
-.controller('InFlightController', function($scope, $ionicModal, $state) {
+.controller('InFlightController', function($scope, $ionicModal, $state, FinnairApi) {
 
   $ionicModal.fromTemplateUrl('views/in-flight/notification.html', {
     scope: $scope,
@@ -9,13 +9,25 @@ angular.module('starter')
     $scope.modal = modal;
   });
 
+  $scope.booking = null;
+
+  FinnairApi.init().then(function() {
+    FinnairApi.getProfile().success(function(data) {
+      FinnairApi.getBookingDetails(data.bookings[0].PNR).success(function(data){
+        $scope.booking = data;
+      });
+    });
+  });
+
   $scope.showNotification = function() {
     $scope.modal.show();
   };
 
   $scope.goToNextFlightInfo = function() {
     $scope.modal.hide();
-    $state.go('tab.flight');
+    $state.go('tab.flight', {
+      flight: $scope.booking.journeys[0].flights[1]
+    });
   };
 
 });
