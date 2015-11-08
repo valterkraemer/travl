e6a545db85f571600a631d17c68ec87ae10baf9b
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2015-11-08 11:06:18 +0200

    get started first, login then

--- www/js/app.js
+++ www/js/app.js
@@ -144,6 +144,6 @@ angular.module('starter', ['ionic', 'ngResource', 'ngCordova'])
   });*/
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/login');
+  $urlRouterProvider.otherwise('/getStarted');
 
 });
