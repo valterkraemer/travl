d549c76f65fe21f844c1f2c3a7671cc2ca96b7ef
Mikael Stenstrand <stenstrand.mikael@gmail.com>
2015-11-08 13:09:47 +0200

    notification at first flight

--- www/views/flight/flight.controller.js
+++ www/views/flight/flight.controller.js
@@ -9,4 +9,36 @@ angular.module('starter')
     $rootScope.points -= points;
   };
 
+  $ionicModal.fromTemplateUrl('views/in-flight/notification.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.booking = null;
+
+  FinnairApi.init().then(function() {
+    FinnairApi.getProfile().success(function(data) {
+      FinnairApi.getBookingDetails(data.bookings[0].PNR).success(function(data){
+        $scope.booking = data;
+      });
+    });
+  });
+
+  $scope.showNotification = function(flight) {
+    if (flight.uniqueId = "0742015-11-14NRTHEL")  {
+      $scope.modal.show();
+    }
+  };
+
+  $scope.goToNextFlightInfo = function() {
+    $scope.modal.hide();
+    $state.go('tab.airport', {
+      airport: $scope.booking.journeys[0].flights[0].arrival
+    });
+  };
+
+
+
 });
